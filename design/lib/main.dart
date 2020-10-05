2dca4deed5073aed4c3930ab96dc0b476ec06156
vaughan189 <vaughan188@gmail.com>
2020-10-05 13:41:11 +0530

    Use themes to share colors and font styles

--- design/lib/main.dart
+++ design/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,43 +8,57 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final appTitle = 'Orientation Demo';
+    final appName = 'Custom Themes';
 
     return MaterialApp(
-      title: appTitle,
-      home: OrientationList(
-        title: appTitle,
+      title: appName,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.lightBlue[800],
+        accentColor: Colors.cyan[600],
+        fontFamily: 'Georgia',
+        textTheme: TextTheme(
+          headline1: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
+          bodyText2: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
+        ),
+      ),
+      home: MyHomePage(
+        title: appName,
       ),
     );
   }
 }
 
-class OrientationList extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
   final String title;
 
-  OrientationList({Key key, this.title}) : super(key: key);
+  MyHomePage({Key key, @required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
-      body: OrientationBuilder(
-        builder: (context, orientation) {
-          return GridView.count(
-            // Create a grid with 2 columns in portrait mode, or 3 columns in
-            // landscape mode.
-            crossAxisCount: orientation == Orientation.portrait ? 2 : 3,
-            // Generate 100 widgets that display their index in the List.
-            children: List.generate(100, (index) {
-              return Center(
-                child: Text(
-                  'Item $index',
-                  style: Theme.of(context).textTheme.headline1,
-                ),
-              );
-            }),
-          );
-        },
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Container(
+          color: Theme.of(context).accentColor,
+          child: Text(
+            'Text with a background color',
+            style: Theme.of(context).textTheme.headline6,
+          ),
+        ),
+      ),
+      floatingActionButton: Theme(
+        data: Theme.of(context).copyWith(
+          colorScheme:
+              Theme.of(context).colorScheme.copyWith(secondary: Colors.yellow),
+        ),
+        child: FloatingActionButton(
+          onPressed: null,
+          child: Icon(Icons.add),
+        ),
       ),
     );
   }
