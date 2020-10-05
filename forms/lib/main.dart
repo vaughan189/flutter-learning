8ed89c5e1bd2737f04dd457beab20082ca697224
vaughan189 <vaughan188@gmail.com>
2020-10-05 17:25:59 +0530

    Handle changes to a text field

--- forms/lib/main.dart
+++ forms/lib/main.dart
@@ -1,67 +1,70 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Text Field Focus',
+      title: 'Retrieve Text Input',
       home: MyCustomForm(),
     );
   }
 }
 
+// Define a custom Form widget.
 class MyCustomForm extends StatefulWidget {
-  MyCustomForm({Key key}) : super(key: key);
-
   @override
   _MyCustomFormState createState() => _MyCustomFormState();
 }
 
+// Define a corresponding State class.
+// This class holds data related to the Form.
 class _MyCustomFormState extends State<MyCustomForm> {
-  FocusNode myFocusNode;
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+  final myController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
-    myFocusNode = FocusNode();
+
+    myController.addListener(_printLatestValue);
   }
 
   @override
   void dispose() {
-    myFocusNode.dispose();
+    // Clean up the controller when the widget is removed from the widget tree.
+    // This also removes the _printLatestValue listener.
+    myController.dispose();
     super.dispose();
+  }
+
+  _printLatestValue() {
+    print("Second text field: ${myController.text}");
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Text Field Focus'),
+      appBar: AppBar(
+        title: Text('Retrieve Text Input'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onChanged: (text) {
+                print("First text field: $text");
+              },
+            ),
+            TextField(
+              controller: myController,
+            ),
+          ],
         ),
-        body: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(children: [
-              TextField(
-                autofocus: true,
-              ),
-              // The second text field is focused on when a user taps the
-              // FloatingActionButton.
-              TextField(
-                focusNode: myFocusNode,
-              ),
-            ])),
-        floatingActionButton: FloatingActionButton(
-          // When the button is pressed,
-          // give focus to the text field using myFocusNode.
-          onPressed: () => myFocusNode.requestFocus(),
-          tooltip: 'Focus Second Text Field',
-          child: Icon(Icons.edit),
-        ));
+      ),
+    );
   }
 }
