e42da3173aaa8affb17456da1fc48ca7a8c7fa04
Vaughan Po <vaughan188@gmail.com>
2021-02-08 00:21:59 +0530

    ValueListenableProvider added

--- provider_state_management/lib/main.dart
+++ provider_state_management/lib/main.dart
@@ -6,69 +6,56 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<MyModel>(
-      initialData: MyModel(someValue: 'default value'),
-      create: (context) => getStreamOfMyModel(),
-      child: MaterialApp(
-        home: Scaffold(
-          appBar: AppBar(title: Text('My App')),
-          body: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.all(20),
-                color: Colors.green[200],
-                child: Consumer<MyModel>(
-                  builder: (context, myModel, child) {
-                    return RaisedButton(
-                      child: Text('Do something'),
-                      onPressed: () {
-                        myModel.doSomething();
-                      },
-                    );
-                  },
+    return Provider<MyModel>(
+      create: (context) => MyModel(),
+      child: Consumer(
+        builder: (context, myModel, child) {
+          return ValueListenableProvider<String>.value(
+            value: myModel.someValue,
+            child: MaterialApp(
+              home: Scaffold(
+                appBar: AppBar(title: Text('My App')),
+                body: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      padding: const EdgeInsets.all(20),
+                      color: Colors.green[200],
+                      child: Consumer<MyModel>(
+                        builder: (context, myModel, child) {
+                          return RaisedButton(
+                            child: Text('Do something'),
+                            onPressed: () {
+                              myModel.doSomething();
+                            },
+                          );
+                        },
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.all(35),
+                      color: Colors.blue[200],
+                      child: Consumer<String>(
+                        builder: (context, myValue, child) {
+                          return Text(myValue);
+                        },
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              Container(
-                padding: const EdgeInsets.all(35),
-                color: Colors.blue[200],
-                child: Consumer<MyModel>(
-                  builder: (context, myModel, child) {
-                    return Text(myModel.someValue);
-                  },
-                ),
-              ),
-            ],
-          ),
-        ),
+            ),
+          );
+        },
       ),
     );
   }
 }
 
-Stream<MyModel> getStreamOfMyModel() {
-  return Stream<MyModel>.periodic(
-      Duration(seconds: 1), (x) => MyModel(someValue: '$x')).take(10);
-}
-
 class MyModel {
-  MyModel({this.someValue});
-  String someValue = 'Hello';
+  ValueNotifier<String> someValue = ValueNotifier('Hello');
   void doSomething() {
-    someValue = 'Goodbye';
-    print(someValue);
-  }
-}
-
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final myModel = Provider.of<MyModel>(context, listen: false);
-    return RaisedButton(
-      child: Text('Do something'),
-      onPressed: () {
-        myModel.doSomething();
-      },
-    );
+    someValue.value = 'Goodbye';
+    print(someValue.value);
   }
 }
