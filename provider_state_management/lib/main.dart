d31e4a1cd62cd1d16e8b365988face97d054e693
Vaughan Po <vaughan188@gmail.com>
2021-02-07 23:26:59 +0530

    Provider widget added

---
+++ provider_state_management/lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Provider<MyModel>(
+      create: (context) => MyModel(),
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text('My App')),
+          body: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(20),
+                color: Colors.green[200],
+                child: Consumer<MyModel>(
+                  builder: (context, myModel, child) {
+                    return RaisedButton(
+                      child: Text('Do something'),
+                      onPressed: () {
+                        myModel.doSomething();
+                      },
+                    );
+                  },
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.all(35),
+                color: Colors.blue[200],
+                child: Consumer(
+                  builder: (context, myModel, child) {
+                    return Text(myModel.someValue);
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MyModel {
+  String someValue = 'Hello';
+  void doSomething() {
+    someValue = 'Goodbye';
+    print(someValue);
+  }
+}
