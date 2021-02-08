5fe9deedef17924b4cca605e57576abe59464ee1
Vaughan Po <vaughan188@gmail.com>
2021-02-08 16:09:58 +0530

    MultiProvider added

--- provider_state_management/lib/main.dart
+++ provider_state_management/lib/main.dart
@@ -6,56 +6,95 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Provider<MyModel>(
-      create: (context) => MyModel(),
-      child: Consumer(
-        builder: (context, myModel, child) {
-          return ValueListenableProvider<String>.value(
-            value: myModel.someValue,
-            child: MaterialApp(
-              home: Scaffold(
-                appBar: AppBar(title: Text('My App')),
-                body: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      color: Colors.green[200],
-                      child: Consumer<MyModel>(
-                        builder: (context, myModel, child) {
-                          return RaisedButton(
-                            child: Text('Do something'),
-                            onPressed: () {
-                              myModel.doSomething();
-                            },
-                          );
-                        },
-                      ),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<MyModel>(create: (context) => MyModel()),
+        ChangeNotifierProvider<AnotherModel>(
+            create: (context) => AnotherModel()),
+      ],
+      child: MaterialApp(
+        home: Scaffold(
+          appBar: AppBar(title: Text('My App')),
+          body: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.green[200],
+                    child: Consumer<MyModel>(
+                      builder: (context, myModel, child) {
+                        return RaisedButton(
+                          child: Text('Do something'),
+                          onPressed: () {
+                            myModel.doSomething();
+                          },
+                        );
+                      },
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(35),
-                      color: Colors.blue[200],
-                      child: Consumer<String>(
-                        builder: (context, myValue, child) {
-                          return Text(myValue);
-                        },
-                      ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(35),
+                    color: Colors.blue[200],
+                    child: Consumer<MyModel>(
+                      builder: (context, myModel, child) {
+                        return Text(myModel.someValue);
+                      },
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
-            ),
-          );
-        },
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.all(20),
+                    color: Colors.red[200],
+                    child: Consumer<AnotherModel>(
+                      builder: (context, myModel, child) {
+                        return RaisedButton(
+                          child: Text('Do something'),
+                          onPressed: () {
+                            myModel.doSomething();
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(35),
+                    color: Colors.yellow[200],
+                    child: Consumer<AnotherModel>(
+                      builder: (context, anotherModel, child) {
+                        return Text('${anotherModel.someValue}');
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-class MyModel {
-  ValueNotifier<String> someValue = ValueNotifier('Hello');
+class MyModel with ChangeNotifier {
+  String someValue = 'Hello';
   void doSomething() {
-    someValue.value = 'Goodbye';
-    print(someValue.value);
+    someValue = 'Goodbye';
+    print(someValue);
+    notifyListeners();
+  }
+}
+
+class AnotherModel with ChangeNotifier {
+  int someValue = 0;
+  void doSomething() {
+    someValue = 5;
+    print(someValue);
+    notifyListeners();
   }
 }
